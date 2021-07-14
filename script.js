03fde9a1287e351d6555847e96387b786ba5c972
Rodrigo Vianna <roodrigo.vs123@hotmail.com>
2021-07-14 16:39:04 -0300

    Add sass

--- script.js
+++ script.js
@@ -5,40 +5,38 @@ function initAnimaScroll() {
     articles.forEach((article) => {
       const position = article.getBoundingClientRect();
       if (position.top >= 0 && position.bottom <= window.innerHeight) {
-        article.classList.add('active', article.dataset.anime);
+        article.classList.add("active", article.dataset.anime);
       }
-    })
+    });
   }
 
   animaScroll();
-  window.addEventListener('scroll', animaScroll)
+  window.addEventListener("scroll", animaScroll);
 }
 initAnimaScroll();
 
 function initFav() {
   const favIcons = document.querySelectorAll('[data-tab="content"] i');
   favIcons.forEach((icon) => {
-    icon.addEventListener('click', () => {
-      icon.classList.toggle('fas');
-    })
-  })
+    icon.addEventListener("click", () => {
+      icon.classList.toggle("fas");
+    });
+  });
 }
-initFav()
+initFav();
 
 function initDarkMode() {
-  const themeIcon = document.querySelector('.header__nav i');
-  themeIcon.addEventListener('click', () => {
-    if (document.body.dataset.theme === 'light') {
-      document.body.dataset.theme = 'dark';
-      themeIcon.classList.remove('fa-sun');
-      themeIcon.classList.add('fa-moon');
+  const themeIcon = document.querySelector(".header__nav i");
+  themeIcon.addEventListener("click", () => {
+    if (document.body.dataset.theme === "light") {
+      document.body.dataset.theme = "dark";
+      themeIcon.classList.remove("fa-sun");
+      themeIcon.classList.add("fa-moon");
     } else {
-      document.body.dataset.theme = 'light';
-      themeIcon.classList.remove('fa-moon');
-      themeIcon.classList.add('fa-sun');
+      document.body.dataset.theme = "light";
+      themeIcon.classList.remove("fa-moon");
+      themeIcon.classList.add("fa-sun");
     }
-
-
-  })
+  });
 }
 initDarkMode();
